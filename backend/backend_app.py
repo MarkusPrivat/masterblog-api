697e62b4c7a8d4970b212398c5db0872b6754bf8
Markus <25428460+MarkusPrivat@users.noreply.github.com>
2026-03-05 12:47:02 +0100

    /api/posts/ add methods POST to add a post after it is validated.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,33 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+def validate_blog_post(data) -> tuple[bool, str]:
+    if not len(data) == 2:
+        return False, "Incorrect number of data in .JSON!"
+    if 'title' not in data:
+        return False, "'title' not in .JSON!"
+    if 'content' not in data:
+        return False, "'content' not in .JSON!"
+    return True, "Done!"
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    return jsonify(POSTS)
+    if request.method == 'GET':
+        return jsonify(POSTS)
+
+    # method = POST
+    new_post = request.get_json()
+    is_executed, msg = validate_blog_post(new_post)
+    if not is_executed:
+        return jsonify({'error': f"Invalid blog post: {msg}"}), 400
+
+    new_id = max(post['id'] for post in POSTS) + 1
+    new_post['id'] = new_id
+
+    POSTS.append(new_post)
+    return jsonify(new_post), 201
+
 
 
 if __name__ == '__main__':
