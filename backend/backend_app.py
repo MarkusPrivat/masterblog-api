56958fe4c39bf34a424475d9c00002035bfb5e57
Markus <25428460+MarkusPrivat@users.noreply.github.com>
2026-03-05 14:41:43 +0100

    /api/posts/<int:id> methode PUT to edit posts

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -26,6 +26,10 @@ def fetch_post_by_id(id) -> tuple[bool, dict]:
             return True, post
     return False, {'error': f"No post with ID: '{id}'"}
 
+
+def get_new_id() -> int:
+    return max(post['id'] for post in POSTS) + 1
+
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
     if request.method == 'GET':
@@ -37,21 +41,32 @@ def get_posts():
     if not is_executed:
         return jsonify({'error': f"Invalid blog post: {msg}"}), 400
 
-    new_id = max(post['id'] for post in POSTS) + 1
-    new_post['id'] = new_id
+    new_post['id'] = get_new_id()
 
     POSTS.append(new_post)
     return jsonify(new_post), 201
 
 
-@app.route('/api/posts/<int:id>', methods=['DELETE'])
+@app.route('/api/posts/<int:id>', methods=['DELETE', 'PUT'])
 def delete_post(id):
-    is_executed, del_post = fetch_post_by_id(id)
+    is_executed, post = fetch_post_by_id(id)
     if not is_executed:
-        return jsonify(del_post), 404
-    POSTS.remove(del_post)
+        return jsonify(post), 404
 
-    return jsonify({'message': f"Post with ID: '{id}' has been deleted successfully"}), 200
+    if request.method == 'DELETE':
+        POSTS.remove(post)
+        return jsonify({'message': f"Post with ID: '{id}' has been deleted successfully"}), 200
+
+    # method PUT
+    for post_to_edit in POSTS:
+        if post_to_edit['id'] == id:
+            new_post = request.get_json()
+            is_executed, msg = validate_blog_post(new_post)
+            if not is_executed:
+                return jsonify({'error': f"Invalid blog post: {msg}"}), 400
+            post_to_edit.update(new_post)
+            return jsonify(post_to_edit), 200
+
 
 
 if __name__ == '__main__':
